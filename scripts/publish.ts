2bf9e8b190167a21f9e727b5959b930e770f7d87
hboot <bobolity@163.com>
2025-03-24 19:08:23 +0800

    chore: release v0.1.0

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,7 +1,8 @@
 import { execSync } from "node:child_process";
 import { version } from "../package.json";
 
-let command = "pnpm publish -r --access public --no-git-checks";
+let command =
+  "pnpm publish -r --access public --no-git-checks --registry https://registry.npmjs.org";
 
 if (version.includes("beta")) command += " --tag beta";
 
